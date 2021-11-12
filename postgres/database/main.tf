c7b92b2c64261a20b85a9ee13dba4eaf0ec144cb
Weyder Santos <weyder.santos@guiabolso.com.br>
2021-11-12 12:27:02 -0300

    ajust folder

--- postgres/database/main.tf
+++
@@ -1,8 +0,0 @@
-resource "postgresql_database" "pdb" {
-  name              = var.name
-  owner             = var.owner
-  template          = var.template
-  lc_collate        = var.lc_collate
-  connection_limit  = var.connection_limit
-  allow_connections = var.allow_connections
-}
--- postgres/database/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  value       = postgresql_database.pdb.name
-  description = "The name of Database"
-}
-
-output "owner" {
-  value       = postgresql_database.pdb.owner
-  description = "The owner of Database"
-}
